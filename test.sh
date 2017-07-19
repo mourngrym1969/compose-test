5fe7168075b64f440c7019153c6996720144bfbc
Terrence T. Cox <terrencecox@gmail.com>
2017-07-18 21:30:20 -0500

    Starting the docker-compose build and startup in test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
+/usr/bin/docker-compose up
+
 /usr/bin/redis-cli -h 172.18.0.2 --scan --pattern "redis"
 
-if [[ $? = "0" ]]; then
-	exit 0
-else
-	exit 60
-fi
+#if [[ $? = "0" ]]; then
+#	exit 0
+#else
+#	exit 60
+#fi
