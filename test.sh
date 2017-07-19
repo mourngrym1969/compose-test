03381cf85d9678d6ec3831f11b156d23c9c9144a
Terrence T. Cox <terrencecox@gmail.com>
2017-07-18 21:35:08 -0500

    Final Build - getting tired, full travis yaml structure

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-docker-compose up
-
 /usr/bin/redis-cli -h 172.18.0.2 --scan --pattern "redis"
 
-#if [[ $? = "0" ]]; then
-#	exit 0
-#else
-#	exit 60
-#fi
+if [[ $? = "0" ]]; then
+	echo "Yay"
+	exit 0
+else
+	echo "Boo"
+	exit 60
+fi
